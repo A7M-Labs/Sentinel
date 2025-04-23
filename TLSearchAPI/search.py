2cc96787b081499cb0ae28ab86522f50967c5c61
The007Programmer <aahils2019@gmail.com>
2025-04-23 01:35:23 -0700

    TEST: update search.py

--- TLSearchAPI/search.py
+++ TLSearchAPI/search.py
@@ -11,16 +11,16 @@ client = TwelveLabs(api_key=os.getenv('TWELVELABS_API_KEY'))
 video_dict={
 
     "normal1.mp4":"68088a3c352908d3bc50a428",
-    "normal2.mp4":"`",
-    "normal3.mp4":"`",
-    "normal4.mp4":"`",
-    "normal5.mp4":"`",
+    "normal2.mp4":"680897cc352908d3bc50a442",
+    "normal3.mp4":"68089816669d2e9f3f513bb2",
+    "normal4.mp4":"6808a21a352908d3bc50a45b",
+    "normal5.mp4":"6808a26c02327bef162a41a8",
 
-    "rob1.mp4":"`",
-    "rob2.mp4":"`",
-    "rob3.mp4":"`",
-    "rob4.mp4":"`",
-    "rob5.mp4":"`",
+    "rob1.mp4":"6808a2bc02327bef162a41ad",
+    "rob2.mp4":"6808a2f9045da61d81570784",
+    "rob3.mp4":"6808a33702327bef162a41af",
+    "rob4.mp4":"6808a37d669d2e9f3f513bc5",
+    "rob5.mp4":"6808a49f669d2e9f3f513bda",
 
 }
 
